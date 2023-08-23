678f22e097f60fc9ed46208c9b6b75188143ba12
ztmajor <mylovezju@163.com>
2023-08-23 10:12:33 +0800

    mouse action add sleep time and key press

--- src/utils/mouse_action.py
+++ src/utils/mouse_action.py
@@ -1,9 +1,12 @@
+import time
 import pyautogui
 
 
 def doClick(cx, cy):
     pyautogui.moveTo(cx, cy)
-    pyautogui.click()
+    pyautogui.mouseDown()
+    time.sleep(0.2)
+    pyautogui.mouseUp()
 
     # or
     # pyautogui.click(cx, cy)
@@ -14,6 +17,12 @@ def eliminate(x1, y1, x2 ,y2):
 
     pyautogui.moveTo(x2, y2)
     pyautogui.click()
+
+
+def drag():
+    pyautogui.dragTo(100, 200, button='left')     # drag mouse to X of 100, Y of 200 while holding down left mouse button
+    pyautogui.dragTo(300, 400, 2, button='left')  # drag mouse to X of 300, Y of 400 over 2 seconds while holding down left mouse button
+    pyautogui.drag(30, 0, 2, button='right')   # drag the mouse left 30 pixels over 2 seconds while holding down the right mouse button
 
 
 def get_mouse_position():
